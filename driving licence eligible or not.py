8e77380f33c402855325da0f6b7bb9d99f9ef96d
manas verma <71920045+ManasVerma1357@users.noreply.github.com>
2020-09-26 13:07:16 +0530

    Add files via upload

---
+++ driving licence eligible or not.py
@@ -0,0 +1,13 @@
+''' 24.9.20
+author-manas verma'''
+print("welcome to non sarkari driving license giver,can you  pls enter your age")
+print("pls type the number between 7-100,bee logical :|")
+a = int(input())
+if a<18:
+    print("hamko pata hai ki tum scooty chalate ho par drving license toh nhi denge ham")
+elif a==18:
+    print("we will take a drving test for your age")
+else:
+    print("you are eligible for our non sarkari driving licese")
+
+
